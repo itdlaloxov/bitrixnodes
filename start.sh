795012c2ed59fee0f35203dd935d727eaaf41e46
itdlaloxov <139648940+itdlaloxov@users.noreply.github.com>
2024-06-01 14:44:18 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo "привет"
+
+# Проверяем, существует ли папка cache
+if [ ! -d "cache" ]; then
+    # Если папка не существует, создаем ее
+    mkdir cache
+    echo "Папка cache создана"
+else
+    echo "Папка cache уже существует"
+fi
+
+echo "Привет"
